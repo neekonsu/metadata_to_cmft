4e9dccfe324286dd631f82d82da7cf08e40b64cf
Neekon Saadat <neekonsu@gmail.com>
2020-03-27 13:45:31 -0700

    commented code and cleaned checkError calls

--- metadata_to_cmft.go
+++ metadata_to_cmft.go
@@ -7,12 +7,14 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/jlaffaye/ftp"
 )
 
-func readUrls(path string) []string {
+// Read entries from one column of a csv to []string
+func read(path string, index int8) []string {
 	csvfile, _ := os.Open(path)
 	r := csv.NewReader(csvfile)
 	r.Read()
@@ -22,61 +24,23 @@ func readUrls(path string) []string {
 		if err == io.EOF {
 			break
 		}
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkError("Unable to read line from csv: ", err)
 		outTable = append(outTable, record[2])
 	}
 	return outTable
 }
 
-func readMarks(path string) []string {
-	csvfile, _ := os.Open(path)
-	r := csv.NewReader(csvfile)
-	r.Read()
-	var outTable []string
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		outTable = append(outTable, record[3])
-	}
-	return outTable
-}
-
-func readSampleNames(path string) []string {
-	csvfile, _ := os.Open(path)
-	r := csv.NewReader(csvfile)
-	r.Read()
-	var outTable []string
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		outTable = append(outTable, record[1])
-	}
-	return outTable
-}
-
+// Isolate filepath from full GEO url
 func extractPath(url string) string {
 	// example url: ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM537nnn/GSM537697/suppl
 	slashIndexAfterURL := 7 + strings.Index(url[7:], "/")
 	return url[slashIndexAfterURL:]
 }
 
+// With existing ftp.serverConn, list BED files in ftp path as []string
 func listBedFiles(serverConn *ftp.ServerConn, path string) []string {
 	entries, err := serverConn.List(path)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError("Failed to list path: "+path, err)
 	var filenames []string
 	for _, entry := range entries {
 		if strings.HasSuffix(entry.Name, ".bed.gz") {
@@ -86,12 +50,14 @@ func listBedFiles(serverConn *ftp.ServerConn, path string) []string {
 	return filenames
 }
 
+// Boilerplate error code to avoid repetition.
 func checkError(message string, err error) {
 	if err != nil {
 		log.Fatal(message, err)
 	}
 }
 
+// Copied code from stackoverflow: transpose 2d matrix
 func transpose(slice [][]string) [][]string {
 	xl := len(slice[0])
 	yl := len(slice)
@@ -114,83 +80,89 @@ func main() {
 	var bedNames []string
 	var tmpCMFT [][]string
 	var tmpWgetConf [][]string
-	sampleNames := readSampleNames(csvPath)
-	marks := readMarks(csvPath)
+	var wg sync.WaitGroup
+	sampleNames := read(csvPath, 1)
+	marks := read(csvPath, 3)
 	tmpCMFT = append(tmpCMFT, sampleNames)
 	tmpCMFT = append(tmpCMFT, marks)
 
 	serverConn, err := ftp.Dial("ftp.ncbi.nlm.nih.gov:21", ftp.DialWithTimeout(5*time.Second))
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError("Unable to dial ftp server: ", err)
 
 	err = serverConn.Login("anonymous", "anonymous")
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError("Unable to authenticate ftp server: ", err)
 
 	fmt.Println("ftp server connected!")
 	fmt.Println("extracting BED filenames and URLs")
 
-	// TODO: make link extraction into goroutine to speed up process
 	// export full links
-	for _, url := range readUrls(csvPath) {
-		path := extractPath(url)
-		for _, bedFilename := range listBedFiles(serverConn, path) {
-			tmpString = "ftp://ftp.ncbi.nlm.nih.gov/" + path[1:len(path)-1] + "/" + bedFilename
-			fmt.Printf("extracted full link >>> %s\n", tmpString)
-			fullLinks = append(fullLinks, tmpString)
+	wg.Add(1)
+	go func() {
+		for _, url := range read(csvPath, 2) {
+			path := extractPath(url)
+			for _, bedFilename := range listBedFiles(serverConn, path) {
+				tmpString = "ftp://ftp.ncbi.nlm.nih.gov/" + path[1:len(path)-1] + "/" + bedFilename
+				fmt.Printf("extracted full link >>> %s\n", tmpString)
+				fullLinks = append(fullLinks, tmpString)
+			}
 		}
-	}
-	tmpWgetConf = append(tmpWgetConf, fullLinks)
-
+		tmpWgetConf = append(tmpWgetConf, fullLinks)
+		wg.Done()
+	}()
+	wg.Add(1)
 	// export plain BED files
-	for _, url := range readUrls(csvPath) {
-		path := extractPath(url)
-		for _, bedFilename := range listBedFiles(serverConn, path) {
-			tmpString = bedFilename[:len(bedFilename)-3]
-			fmt.Printf("extracted file name >>> %s\n", tmpString)
-			bedNames = append(bedNames, tmpString)
+	go func() {
+		for _, url := range read(csvPath, 2) {
+			path := extractPath(url)
+			for _, bedFilename := range listBedFiles(serverConn, path) {
+				tmpString = bedFilename[:len(bedFilename)-3]
+				fmt.Printf("extracted file name >>> %s\n", tmpString)
+				bedNames = append(bedNames, tmpString)
+			}
 		}
-	}
-	tmpCMFT = append(tmpCMFT, bedNames)
-
+		tmpCMFT = append(tmpCMFT, bedNames)
+		wg.Done()
+	}()
+	wg.Wait()
 	fmt.Println("Done extracting BED filenames and URLs, disconnecting from ftp server")
 
-	if err := serverConn.Quit(); err != nil {
-		log.Fatal(err)
-	}
+	err = serverConn.Quit()
+	checkError("Unable to disconnect from server: ", err)
 
 	fmt.Println("Disconnected from server, writing files to current directory")
 
 	file1, err := os.Create("cmft.tsv")
-	checkError("error while exporting new cmft.csv:", err)
+	checkError("error while exporting new cmft.csv: ", err)
 	defer file1.Close()
 
 	writer1 := csv.NewWriter(file1)
-	writer1.Comma = '\t'
 	defer writer1.Flush()
 
 	file2, err := os.Create("wget.conf")
-	checkError("error while exporting new cmft.csv:", err)
+	checkError("error while exporting new cmft.csv: ", err)
 	defer file2.Close()
 
 	writer2 := csv.NewWriter(file2)
-	writer2.Comma = '\t'
 	defer writer2.Flush()
 
 	tmpCMFT = transpose(tmpCMFT)
 	tmpWgetConf = transpose(tmpWgetConf)
-
-	for _, value := range tmpCMFT {
-		err := writer1.Write(value)
-		checkError("Cannot write to file", err)
-	}
-
-	for _, value := range tmpWgetConf {
-		err := writer2.Write(value)
-		checkError("Cannot write to file", err)
-	}
-
+	wg.Add(1)
+	go func() {
+		for _, value := range tmpCMFT {
+			err := writer1.Write(value)
+			checkError("Cannot write to file: ", err)
+		}
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		for _, value := range tmpWgetConf {
+			err := writer2.Write(value)
+			checkError("Cannot write to file: ", err)
+		}
+		wg.Done()
+	}()
+	wg.Wait()
 	fmt.Println("All Done!")
 }
